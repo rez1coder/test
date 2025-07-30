02fe4f222a0ca6103964a14c551a420a16a40015
rez1coder <66778119+rez1coder@users.noreply.github.com>
2025-07-31 05:40:51 +0600

    attempt to take input

--- src/main.js
+++ src/main.js
@@ -10,5 +10,19 @@ const riveInstance = new rive.Rive({
 
   onLoad: () => {
     riveInstance.resizeDrawingSurfaceToCanvas();
+
+    const inputs = r.stateMachineInputs('State Machine 1');
+    inputs.forEach(i => {
+        const inputName = i.name;
+        const inputType = i.type;
+        switch(inputType) {
+            case rive.StateMachineInputType.Trigger:
+                i.fire();
+                break;
+            case rive.StateMachineInputType.Number:
+                i.value = 20;
+                break;
+        }
+    });
   },
 });
