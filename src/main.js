cf37f577aba96e81a038a61471d6d12f2a2d953a
rez1coder <66778119+rez1coder@users.noreply.github.com>
2025-07-31 05:49:08 +0600

    attempt 2 to take input

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // This is the High level JS runtime for Rive
 // https://rive.app/community/doc/web-js/docvlgbnS1mp
 
+const params = new URLSearchParams(window.location.search);
+const fireFlag = params.get("flag");
+
 const riveInstance = new rive.Rive({
   src: "flag.riv",
   canvas: document.getElementById("canvas"),
@@ -11,18 +14,17 @@ const riveInstance = new rive.Rive({
   onLoad: () => {
     riveInstance.resizeDrawingSurfaceToCanvas();
 
-    const inputs = r.stateMachineInputs('State Machine 1');
-    inputs.forEach(i => {
-        const inputName = i.name;
-        const inputType = i.type;
-        switch(inputType) {
-            case rive.StateMachineInputType.Trigger:
-                i.fire();
-                break;
-            case rive.StateMachineInputType.Number:
-                i.value = 20;
-                break;
-        }
-    });
+    const inputs = riveInstance.stateMachineInputs("State Machine 1");
+
+    const winSide = inputs.find(i => i.name === "Win Side");
+    const winFront = inputs.find(i => i.name === "Win Front");
+    const flagType = inputs.find(i => i.name === "Flag Type");
+
+    if (winSide)  winSide.value  = 20;
+    if (winFront) winFront.value = 20;
+
+    if (flagType && fireFlag) {
+      flagType.fire();
+    }
   },
 });
